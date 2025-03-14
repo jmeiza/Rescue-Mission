fc77cb7ffd83d3487c7b3889690827907761edb1
gmcga <gmcgaughey@live.com>
2025-03-13 20:54:08 -0400

    Update package name to be team40 from teamXXX

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team40/Creek.java
@@ -0,0 +1,8 @@
+package ca.mcmaster.se2aa4.island.team40;
+
+// some algorithm for finding the creek
+
+public class Creek 
+{
+    
+}
--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/EmergencySite.java
+++
@@ -1,8 +0,0 @@
-package ca.mcmaster.se2aa4.island.teamXXX;
-
-// some algorithm for finding the site
-
-public class EmergencySite 
-{
-    
-}
